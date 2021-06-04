c56623663017d27ab3ea82adae59fdab22c10bb1
Richard Cranwell <rickycranwell@gmail.com>
2021-06-04 13:28:11 -0500

    cell color changes on hover

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
+function hoverEvent(e){
+    this.classList.add('hovered');
+}
+
+
+
+
+
+
 let size = 16;
 const container = document.querySelector('#container');
 for(let i = 0; i < size; i++){
@@ -6,6 +15,7 @@ for(let i = 0; i < size; i++){
     for(let j = 0; j < size; j++){
         let cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.addEventListener('mouseenter', hoverEvent,{once : true});
         row.appendChild(cell);
     }
     container.appendChild(row);
